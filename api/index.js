2c5248f1d5f7530cfe2934762a7e50959ff26a59
Matheus <matheus_julidori@yahoo.com.br>
2021-08-23 14:16:29 -0300

    Initial configs

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const config = require('config');
+
+app.use(express.json())
+
+
+app.listen(config.get('api.port'), () => {
+    console.log('listening on port ' + port);
+});
