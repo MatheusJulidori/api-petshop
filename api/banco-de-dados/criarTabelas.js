6f6a6c7221843f9462ef54280414fea712e75fd0
Matheus <matheus_julidori@yahoo.com.br>
2021-08-25 11:47:16 -0300

    Filter By ID

--- api/banco-de-dados/criarTabelas.js
+++ api/banco-de-dados/criarTabelas.js
@@ -1,6 +1,6 @@
 const ModeloTabela = require('../rotas/fornecedores/ModeloTabelaFornecedor')
 
 ModeloTabela
-    .sync()
+    .async()
     .then(() => console.log('Tabela criada com sucesso'))
     .catch(console.log)
